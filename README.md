b6eeb3c6278a3a3fbe92dbb7aa0b2538386e3e52
Joseph Carr <joseph.carr@mail.mcgill.ca>
2020-07-11 00:33:28 -0400

    A first basic version

---
+++ README.md
@@ -0,0 +1,32 @@
+# cyberspace
+A WebGL project to depict cyberspace as a rendering of source code on flying planes in 3D.
+
+## Features
+- WebGL GPU accelerated rendering
+- Simplified plane transformations in terms of polar rotations, azimuthal rotations, and translations
+- HTML5 canvas used to render the text textures
+- The program's own GLSL source code is rendered
+
+## Implementation
+At a high level the program essentially works like this:  
+1. Draw the shader source code on a 2d HTML5 canvas
+2. Convert the image to a WebGL texture
+3. Define the basic plane and texture it with the source code image
+4. Define the transformation of the plane
+5. Draw the plane in 3D space
+6. Repleat steps 4&5 for as many planes as desired
+
+
+## WebGL resources
+### Complete Guides
+https://webglfundamentals.org  
+http://learnwebgl.brown37.net  
+https://xem.github.io/articles/webgl-guide.html  
+### Introductory Examples
+https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Basic_2D_animation_example  
+https://www.tutorialspoint.com/webgl/webgl_drawing_points.htm  
+### Mathematics Resources
+https://en.wikipedia.org/wiki/Rotation_matrix  
+https://en.wikipedia.org/wiki/Spherical_coordinate_system  
+https://en.wikipedia.org/wiki/Affine_transformation  
+https://en.wikipedia.org/wiki/Homogeneous_coordinates  
---
+++ scripts/cyberspace.js
@@ -0,0 +1,253 @@
+// A script for simulating particles with WebGL
+
+// Define globals
+let gl = null;
+let glCanvas = null;
+let shaderProgram;
+
+window.addEventListener("load", main, false);
+
+
+
+
+// Puts text in center of canvas.
+function makeTextCanvas(text, width, height) {
+	var textCanvas = document.createElement("canvas").getContext("2d");
+	//var textCanvas = document.getElementById("glcanvas").getContext("2d");
+	textCanvas.canvas.width  = width;
+	textCanvas.canvas.height = height;
+	textCanvas.font = "20px monospace";
+	textCanvas.fillStyle = "blue";
+	textCanvas.clearRect(0, 0, width, height);
+	text = text.split("\n");
+	for (var i_line=0; i_line < text.length; i_line++){
+		textCanvas.fillText(text[i_line], 20, (i_line+2)*20);
+	}
+	return textCanvas.canvas;
+}
+
+
+// The programs
+let vertexShader = `
+	const mat4 perspective_transform = mat4(
+		1,0,0,0,
+		0,-1,0,0,
+		0,0,1,0,
+		0,0,1,0
+	);
+
+	uniform float azimuthal_angle;
+	uniform float polar_angle;
+	uniform vec3 translation;
+
+
+	attribute vec2 a_position;
+	attribute vec2 a_texCoord;
+
+	uniform vec3 clipspace_scale;
+
+	varying vec2 v_texCoord;
+
+	void main() {
+		// First transform the position in viewspace
+		vec3 viewspace_position = vec3(a_position, 0);
+		
+		// Azimuthal rotation
+		float rad_azimuthal_angle = radians(azimuthal_angle);
+		mat3 azimuthal_rotation = mat3(
+			1, 0, 0,
+			0, cos(rad_azimuthal_angle), -sin(rad_azimuthal_angle),
+			0, sin(rad_azimuthal_angle), cos(rad_azimuthal_angle)
+			);
+		viewspace_position = viewspace_position * azimuthal_rotation;
+		// Polar rotation
+		float rad_polar_angle = radians(polar_angle);
+		mat3 polar_rotation = mat3(
+			cos(rad_polar_angle), 0, -sin(rad_polar_angle),
+			0, 1, 0,
+			sin(rad_polar_angle), 0, cos(rad_polar_angle)
+			);
+		viewspace_position = viewspace_position * polar_rotation;
+		// Translation
+		viewspace_position += translation;
+
+
+		// Then set the clipspace position
+		vec4 clipspace_position = vec4(2.0 * viewspace_position / clipspace_scale, 1);
+		gl_Position = clipspace_position * perspective_transform;
+
+		// pass the texCoord to the fragment shader
+		// The GPU will interpolate this value between points.
+		v_texCoord = a_texCoord;
+	}
+`;
+let fragmentShader = `
+	precision mediump float;
+
+	// our texture
+	uniform sampler2D u_image;
+	// the texCoords passed in from the vertex shader.
+	varying vec2 v_texCoord;
+
+	void main() {
+		gl_FragColor = texture2D(u_image, v_texCoord);
+	}
+`;
+
+
+function main() {
+	// Before the WebGL part let's make the texture
+	var image = makeTextCanvas(vertexShader+fragmentShader, 800, 800)
+
+	// Initial setup
+	glCanvas = document.getElementById("glcanvas");
+	gl = glCanvas.getContext("webgl");
+	// Set the view port
+	gl.viewport(0,0, glCanvas.width, glCanvas.height);
+	gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true);
+	gl.enable(gl.BLEND);
+	gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
+	//gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+	//gl.depthMask(false);
+	
+
+	const shaderSet = [
+		{
+			type: gl.VERTEX_SHADER,
+			source: vertexShader
+		},
+		{
+			type: gl.FRAGMENT_SHADER,
+			source: fragmentShader
+		}
+	];
+	program = buildShaderProgram(shaderSet);
+	gl.useProgram(program);
+	
+	
+	
+	// Setup data
+	var n_dim = 2;
+	var n_tris = 2;
+	
+	// Uniform indicies
+	var clipspace_scaleLocation = gl.getUniformLocation(program, "clipspace_scale");
+	var azimuthal_angleLocation = gl.getUniformLocation(program, "azimuthal_angle");
+	var polar_angleLocation = gl.getUniformLocation(program, "polar_angle");
+	var translationLocation = gl.getUniformLocation(program, "translation");
+	// Attributes indicies
+	var positionLocation = gl.getAttribLocation(program, "a_position");
+	var texcoordLocation = gl.getAttribLocation(program, "a_texCoord");
+	// Buffer allocation and indicies
+	var positionBuffer = gl.createBuffer();
+	var texcoordBuffer = gl.createBuffer();
+	// Texture allocation and indicies
+	var texture = gl.createTexture();
+	
+	
+	// Create a buffer to put three 2d clip space points in
+	var rectangleCoordinates = [
+		-image.width/2, -image.height/2,
+		image.width/2, -image.height/2,
+		-image.width/2, image.height/2,
+		-image.width/2, image.height/2,
+		image.width/2, -image.height/2,
+		image.width/2, image.height/2,
+	];
+	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(rectangleCoordinates), gl.STATIC_DRAW);
+
+	// provide texture coordinates for the rectangle.
+	var textureCoordinates = [
+		0,  0,
+		1,  0,
+		0,  1,
+		0,  1,
+		1,  0,
+		1,  1,
+	];
+	gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
+
+
+	// Create a texture.
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+
+	// Tell WebGL how to convert from clip space to pixels
+	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+	// Clear the canvas
+	gl.clearColor(0, 0, 0, 1);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+
+
+	// Turn on the position attribute
+	gl.enableVertexAttribArray(positionLocation);
+	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+	gl.vertexAttribPointer(positionLocation, n_dim, gl.FLOAT, false, 0, 0);
+
+	// Turn on the texcoord attribute
+	gl.enableVertexAttribArray(texcoordLocation);
+	gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+	gl.vertexAttribPointer(texcoordLocation, n_dim, gl.FLOAT, false, 0, 0);
+
+	// set the resolution
+	gl.uniform3f(clipspace_scaleLocation, gl.canvas.width, gl.canvas.height, (gl.canvas.width+gl.canvas.height)/2);
+
+	function drawPlane(azimuthal_angle, polar_angle, x_translation, y_translation, z_translation){
+		// set the transform
+		gl.uniform1f(azimuthal_angleLocation, azimuthal_angle);
+		gl.uniform1f(polar_angleLocation, polar_angle);
+		gl.uniform3f(translationLocation, x_translation, y_translation, z_translation);
+
+		// Draw the rectangle.
+		gl.drawArrays(gl.TRIANGLES, 0, 3*2);
+	}
+
+	drawPlane(0, 0, -100, 0, 500);
+	drawPlane(-25, 30, 300, -100, 700);
+}
+
+
+
+
+// Shader building tools from Mozilla
+function buildShaderProgram(shaderInfo) {
+	let program = gl.createProgram();
+
+	shaderInfo.forEach(function(desc) {
+		let shader = compileShader(desc.source, desc.type);
+		if (shader) gl.attachShader(program, shader);
+	});
+
+	gl.linkProgram(program)
+
+	if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+		console.log("Error linking shader program:");
+		console.log(gl.getProgramInfoLog(program));
+	}
+
+	return program;
+}
+
+function compileShader(source, type) {
+	let shader = gl.createShader(type);
+
+	gl.shaderSource(shader, source);
+	gl.compileShader(shader);
+
+	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+		console.log(`Error compiling ${type === gl.VERTEX_SHADER ? "vertex" : "fragment"} shader:`);
+		console.log(gl.getShaderInfoLog(shader));
+	}
+	return shader;
+}
+
+
+
